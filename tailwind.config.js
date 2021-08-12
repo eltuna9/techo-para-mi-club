117e37db24f3966fb3a591fbdefc46c352175746
eltuna9 <antonioherrera990@gmail.com>
2021-08-13 08:13:14 +1000

    Add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#1B35A5',
+        },
+        secondary: {
+          DEFAULT: '#4BC5EF',
+        },
+        tertiary: {
+          DEFAULT: '#F8BE37',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
