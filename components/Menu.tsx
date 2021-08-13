3e6c49b65e5b39f1c0dae5216e598c9707f2d6ed
floracodes <flor.calvo89@gmail.com>
2021-08-14 09:12:55 +1000

    Fix svg properties

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -71,9 +71,9 @@ export default function Menu() {
                 className=" w-8 h-8 p-1 rounded-md bg-blue-800 text-white "
                 x-show="!showMenu"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
