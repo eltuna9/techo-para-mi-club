89c8703d321f52712250dae847e8f9b3a0b0d8fc
floracodes <flor.calvo89@gmail.com>
2021-08-14 10:16:43 +1000

    Add styles and close icon

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Image from 'next/dist/client/image'
+import { IoCloseSharp, IoMenu } from 'react-icons/io5'
 
 export default function Menu() {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -9,19 +10,18 @@ export default function Menu() {
   }
 
   return (
-    <nav className="bg-white m-auto w-full md:w-11/12 md:rounded-full">
-      <div className="max-w-6xl mx-auto px-12">
+    <nav className="bg-white m-auto w-full lg:w-11/12 xl:w-10/12 md:rounded-full">
+      <div className="px-6 md:mx-auto md:px-12">
         <div className="flex justify-between ">
           <div className="flex space-x-7">
             <div>
               <a href="#" className="flex  items-center px-2">
-                {/* <img src="logo.png" alt="Logo" className="h-8 w-8 mr-2" /> */}
                 <span className="flex font-semibold text-blue-800 text-lg">
                   <Image
                     src="/logoCapaniaWide.png"
                     alt="Logo un techo para mi club"
-                    width="200"
-                    height="70"
+                    width="218"
+                    height="71"
                     className="justify-self-center"
                   />
                 </span>
@@ -32,32 +32,32 @@ export default function Menu() {
             <div className="hidden md:flex items-center space-x-1">
               <a
                 href=""
-                className="py-4 px-2 text-blue-500 font-semibold border-blue-500  "
+                className="py-4 px-2 text-xl xl:text-2xl text-blue-500 font-semibold border-blue-500  "
               >
                 Campana
               </a>
               <a
                 href=""
-                className="py-4 px-2 text-blue-800 font-semibold hover:text-blue-500 transition duration-300"
+                className="py-4 px-2 text-xl xl:text-2xl text-blue-800 font-semibold hover:text-blue-500 transition duration-300"
               >
                 Novedades
               </a>
               <a
                 href=""
-                className="py-4 px-2 text-blue-800 font-semibold hover:text-blue-500 transition duration-300"
+                className="py-4 px-2 text-xl xl:text-2xl text-blue-800 font-semibold hover:text-blue-500 transition duration-300"
               >
                 El club
               </a>
               <a
                 href=""
-                className="py-4 px-2 text-blue-800 font-semibold hover:text-blue-500 transition duration-300"
+                className="py-4 px-2 text-xl xl:text-2xl text-blue-800 font-semibold hover:text-blue-500 transition duration-300"
               >
                 Gracias
               </a>
             </div>
             <a
               href=""
-              className="py-2 px-8 font-medium text-primary bg-tertiary font-semibold rounded hover:bg-tertiary-light transition duration-300"
+              className="py-2 px-4 xl:px-8 text-xl xl:text-2xl text-primary bg-tertiary font-semibold rounded hover:bg-tertiary-light transition duration-300"
             >
               Ayudanos
             </a>
@@ -67,18 +67,11 @@ export default function Menu() {
               className="outline-none mobile-menu-button"
               onClick={handleToggle}
             >
-              <svg
-                className=" w-8 h-8 p-1 rounded-md bg-blue-800 text-white "
-                x-show="!showMenu"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
+              {menuOpen ? (
+                <IoCloseSharp className=" w-10 h-10 p-1 rounded-md bg-blue-800 text-white " />
+              ) : (
+                <IoMenu className=" w-10 h-10 p-1 rounded-md bg-blue-800 text-white " />
+              )}
             </button>
           </div>
         </div>
@@ -89,11 +82,11 @@ export default function Menu() {
           ' md:hidden mobile-menu rounded-none px-6 py-8 h-full '
         }
       >
-        <ul className="">
+        <ul className="px-6">
           <li className="active">
             <a
               href="index.html"
-              className="block text-sm px-2 py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
+              className="block text-3xl py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
             >
               Campana
             </a>
@@ -101,7 +94,7 @@ export default function Menu() {
           <li>
             <a
               href="#novedades"
-              className="block text-sm px-2 py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
+              className="block text-3xl py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
             >
               Novedades
             </a>
@@ -109,7 +102,7 @@ export default function Menu() {
           <li>
             <a
               href="#elClub"
-              className="block text-sm px-2 py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
+              className="block text-3xl py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
             >
               El club
             </a>
@@ -117,15 +110,15 @@ export default function Menu() {
           <li>
             <a
               href="#gracias"
-              className="block text-sm px-2 py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
+              className="block text-3xl py-3 text-blue-800 hover:text-blue-500 font-semibold transition duration-300"
             >
               Gracias
             </a>
           </li>
-          <li className="my-4">
+          <li className="my-10">
             <a
               href=""
-              className="py-2 px-8  font-medium text-white bg-blue-400 rounded hover:bg-blue-300 transition duration-300"
+              className="py-2 px-8 text-2xl text-primary bg-tertiary font-semibold rounded hover:bg-tertiary-light transition duration-300"
             >
               Ayudanos
             </a>
