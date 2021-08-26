ea7bbacd658344849711966172a21696360eacf5
Flora <40923958+floraCodes@users.noreply.github.com>
2021-08-26 15:24:57 +1000

    Bug/7gz2db fix donations counter (#12)
    * Fix donations counter for big amounts of evil metal
    
    * Fix padding
    
    * Change padding y

--- components/DonationsCounter.tsx
+++ components/DonationsCounter.tsx
@@ -9,6 +9,7 @@ interface DonationsCounterProps {
 const goalAmount = 20000000
 export function DonationsCounter(props: DonationsCounterProps) {
   const { className, amount } = props
+  const amountTooBig = amount.length > 8
   const percentage = (
     (parseInt(extractNumberFromString(amount), 10) * 100) /
     goalAmount
@@ -19,16 +20,30 @@ export function DonationsCounter(props: DonationsCounterProps) {
       <Stripes />
       <div className="w-full lg:w-10/12 xl:container mx-auto relative z-50">
         <div
-          className={`w-full mx-auto p-4 md:p-10 lg:border-8 lg:rounded-3xl flex justify-center items-center border-t-8 border-b-8 border-white bg-primary text-white shadow-lg`}
+          className={`w-full mx-auto p-4 md:px-10 md:py-8 lg:border-8 lg:rounded-3xl flex justify-center items-center border-t-8 border-b-8 border-white bg-primary text-white shadow-lg`}
         >
-          <div className="flex justify-between w-full flex-wrap">
-            <div className="flex flex-col w-full lg:w-8/12 text-center">
+          <div className="flex justify-between items-center w-full flex-wrap">
+            <div
+              className={`flex flex-col w-full ${
+                amountTooBig ? 'lg:w-7/12' : 'lg:w-8/12'
+              } text-center`}
+            >
               <span className="text-secondary mb-4 text-2xl lg:text-2xl xl:text-3xl font-semibold">
                 Este sueño lleva recaudado
               </span>
-              <span className="text-6xl md:text-8xl font-bold">${amount}</span>
+              <span
+                className={` ${
+                  amountTooBig ? 'md:text-7xl' : 'md:text-8xl'
+                } text-6xl  font-bold`}
+              >
+                ${amount}
+              </span>
             </div>
-            <div className="flex flex-col w-full lg:w-4/12 text-center ">
+            <div
+              className={`flex pl-4 flex-col w-full ${
+                amountTooBig ? 'lg:w-5/12' : 'lg:w-4/12'
+              } text-center `}
+            >
               <span className="text-tertiary pt-4 text-6xl md:text-8xl font-bold">
                 {percentage}%
               </span>
