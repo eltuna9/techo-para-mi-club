d339744ca52ea726f9ee1537b4f24d8e36772a6f
floracodes <flor.calvo89@gmail.com>
2021-09-03 14:33:02 +1000

    Post Card Component

---
+++ components/newsSections/PostCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function PostCardBig() {
+  return (
+    <div className="w-8/12 bg-blue-500 flex">
+      <div className="w-1/3 bg-blue-300">Image</div>
+      <div>Content</div>
+    </div>
+  )
+}
