43d6744327c4e9e01459153691c6bc911c968ecc
eltuna9 <antonioherrera990@gmail.com>
2021-10-31 18:21:44 +1100

    Fix thanks gif css

--- components/helpUsSections/helpUsPaymentSection/donationPaymentForm/PaymentSuccessFull.tsx
+++ components/helpUsSections/helpUsPaymentSection/donationPaymentForm/PaymentSuccessFull.tsx
@@ -9,16 +9,16 @@ export function PaymentSuccessFull(props: PaymentSuccessFullProps) {
   const { t } = useTranslation()
   return (
     <div className="w-full text-center">
-      <h1 className="text-secondary text-lg md:text-5xl text-center mb-4 md:mb-10 font-bold">
+      <h1 className="text-secondary text-lg md:text-4xl text-center mb-4 md:mb-6 font-bold">
         {t('helpUs:paymentThanksTitle')}
       </h1>
 
-      <h2 className="text-md md:text-3xl mb-8">{paymentResultTitle}</h2>
-      <p className="text-sm md:text-2xl mb-10">{paymentResultDescription}</p>
-      <div className="w-4/5 mx-auto">
+      <h2 className="text-md md:text-3xl mb-4">{paymentResultTitle}</h2>
+      <p className="text-sm md:text-xl mb-10">{paymentResultDescription}</p>
+      <div className="w-3/5 mx-auto text-center">
         <img
           src="https://i.giphy.com/media/229vN1LszOgKayj87M/giphy.webp"
-          className="w-100"
+          className="w-10/12 mx-auto"
           alt={t('helpUs:paymentThanksTitle')}
         />
       </div>
