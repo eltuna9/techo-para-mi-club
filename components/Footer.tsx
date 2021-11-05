f80c92c0c1981f0f53b85abc763fbfa9f8e38abc
eltuna9 <antonioherrera990@gmail.com>
2021-11-05 11:06:00 +1100

    Add mq3 signature

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,8 @@ import { FaFacebookF, FaInstagram, FaCaretUp } from 'react-icons/fa'
 import { CaretSquareButton } from './CaretSquareButton'
 import { LogoCampaingWhite } from './svg/logoCampaignWhite'
 import { LogoClub } from './svg/logoClub'
+import Image from 'next/dist/client/image'
+import mq3 from '../public/mq3.png'
 
 export function Footer() {
   const { t } = useTranslation()
@@ -13,52 +15,64 @@ export function Footer() {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
   return (
-    <div className="w-full bg-primary p-10 md:p-16 relative">
-      <CaretSquareButton
-        caretDirection="up"
-        onClick={handleCaretClick}
-        className="absolute -top-14 right-10"
-      />
-      <div className="container flex justify-between flex-wrap mx-auto">
-        <FooterSection>
-          <LogoCampaingWhite className="w-4/6 sm:w-4/6 pb-8" />
-        </FooterSection>
-        <FooterSection>
-          <LogoClub className="w-3/6 md:w-2/6 pb-8" />
-        </FooterSection>
-        <FooterSection>
-          <div className="text-center">
-            <h4 className="text-tertiary text-lg">
-              {t('common:contact').toUpperCase()}
-            </h4>
-            <p className="text-white text-lg">
-              San Juan 1550 - Salta Capital, Argentina
-            </p>
-            <div className="flex pt-4 justify-center">
-              <a
-                href="https://www.instagram.com/general_paz_salta/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IconWrapper>
-                  <FaInstagram className="text-xl" />
-                </IconWrapper>
-              </a>
+    <>
+      <div className="w-full bg-primary p-10 md:p-16 relative">
+        <CaretSquareButton
+          caretDirection="up"
+          onClick={handleCaretClick}
+          className="absolute -top-14 right-10"
+        />
+        <div className="container flex justify-between flex-wrap mx-auto">
+          <FooterSection>
+            <LogoCampaingWhite className="w-4/6 sm:w-4/6 pb-8" />
+          </FooterSection>
+          <FooterSection>
+            <LogoClub className="w-3/6 md:w-2/6 pb-8" />
+          </FooterSection>
+          <FooterSection>
+            <div className="text-center">
+              <h4 className="text-tertiary text-lg">
+                {t('common:contact').toUpperCase()}
+              </h4>
+              <p className="text-white text-lg">
+                San Juan 1550 - Salta Capital, Argentina
+              </p>
+              <div className="flex pt-4 justify-center">
+                <a
+                  href="https://www.instagram.com/general_paz_salta/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IconWrapper>
+                    <FaInstagram className="text-xl" />
+                  </IconWrapper>
+                </a>
 
-              <a
-                href="https://www.facebook.com/Club-Deportivo-General-Paz-437368147029695"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IconWrapper>
-                  <FaFacebookF className="text-xl" />
-                </IconWrapper>
-              </a>
+                <a
+                  href="https://www.facebook.com/Club-Deportivo-General-Paz-437368147029695"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IconWrapper>
+                    <FaFacebookF className="text-xl" />
+                  </IconWrapper>
+                </a>
+              </div>
             </div>
-          </div>
-        </FooterSection>
+          </FooterSection>
+        </div>
       </div>
-    </div>
+      <div className="w-full flex justify-center text-xs bg-gray-200">
+        <a
+          href="https://www.menorque3.com.ar/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {t('common:webSignature')}{' '}
+          <img src="/mq3.png" className="w-24 inline-block" />
+        </a>
+      </div>
+    </>
   )
 }
 
